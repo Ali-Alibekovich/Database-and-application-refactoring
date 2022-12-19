be38f0e7e475687d2055b82bd4445bcea844f3d6
Канторов Всеволод <Vsevolod628@gmail.com>
2022-12-19 14:28:29 +0300

    Добавить форму настроек пользовательского интерфейса и прочее

---
+++ project/src/main/resources/js/components/running-title.vue
@@ -0,0 +1,60 @@
+<template>
+    <header class="header">
+        <b-nav :class="['navbar', isDarkTheme ? 'navbar-dark bg-dark' : 'navbar-light bg-indigo-200']">
+            <b-nav-text>Kantorov&Chupanov&Ilinskaya, P34101</b-nav-text>
+            <span class="secret">www.youtube.com/watch?v=4TOB3tJIUzE</span>
+            <div v-if="isLoggedIn" class="atop">
+                <b-button v-b-modal.modalSettings :variant="[isDarkTheme ? 'outline-secondary' : 'light']">Settings</b-button>
+                <b-button @click="logout" :variant="[isDarkTheme ? 'outline-secondary' : 'light']">Logout</b-button>
+            </div>
+            <div v-else class="atop">
+                <b-button v-if="isDarkTheme" @click="changeTheme" variant="outline-secondary">Light theme</b-button>
+                <b-button v-else @click="changeTheme" variant="light">Dark theme</b-button>
+            </div>
+        </b-nav>
+        <settings/>
+    </header>
+</template>
+
+<script>
+    import {mapActions, mapGetters} from "vuex"
+    import Settings from "../components/settings.vue"
+    export default {
+        name: "running-title",
+        components: {Settings},
+        computed: {...mapGetters(['isLoggedIn', 'isDarkTheme'])},
+        methods: {...mapActions(['logout', 'changeTheme'])}
+    }
+</script>
+
+<style scoped>
+    .secret{
+        position: absolute;
+        transform: translateX(-50%);
+        left: 50%;
+        color: transparent;
+    }
+    .atop{
+        z-index: 11;
+    }
+    .btn-light {
+        color: #212529;
+        background-color: #c29ffa;
+        border-color: #212529;
+    }
+    .btn-light:focus {
+        color: #212529;
+        background-color: #c29ffa;
+        border-color: #212529;
+    }
+    .btn-light:hover {
+        color: #212529;
+        background-color: #e0cffc;
+        border-color: #212529;
+    }
+    .header{
+        position: sticky;
+        top:0;
+        z-index: 10;
+    }
+</style>
---
+++ project/src/main/resources/js/store/modules/customization.js
@@ -0,0 +1,64 @@
+import Customization from '../../auth/customization'
+
+const settings = JSON.parse(localStorage.getItem('settings'))
+const defaultSettings = new Customization(true, 10,'#b8daff', '#000000', '#ff140d', '#dee2e6')
+
+const initialState = settings ? settings : defaultSettings
+
+export const customization = {
+    state: initialState,
+    actions: {
+        changeTheme({commit}){commit('setThemeColor')},
+        changeNumberOfRowInTable({commit}, number){commit('setNumberOfRowInTable', number)},
+        changeAreaColor({commit}, color){commit('setAreaColor', color)},
+        changeHitColor({commit}, color){commit('setHitColor', color)},
+        changeMissColor({commit}, color){commit('setMissColor', color)},
+        changeBackgroundColor({commit}, color){commit('setBackgroundColor', color)},
+    },
+    mutations: {
+        setThemeColor(state){
+            state.isDarkTheme = !state.isDarkTheme
+            localStorage.setItem('settings', JSON.stringify(new Customization(
+            state.isDarkTheme, state.numberOfRowInTable,
+            state.areaColor, state.hitColor, state.missColor, state.backgroundColor)))
+        },
+        setNumberOfRowInTable(state, number){
+            state.numberOfRowInTable = number
+            localStorage.setItem('settings', JSON.stringify(new Customization(
+                state.isDarkTheme, state.numberOfRowInTable,
+                state.areaColor, state.hitColor, state.missColor, state.backgroundColor)))
+        },
+        setAreaColor(state, color){
+            state.areaColor = color
+            localStorage.setItem('settings', JSON.stringify(new Customization(
+                state.isDarkTheme, state.numberOfRowInTable,
+                state.areaColor, state.hitColor, state.missColor, state.backgroundColor)))
+        },
+        setHitColor(state, color){
+            state.hitColor = color
+            localStorage.setItem('settings', JSON.stringify(new Customization(
+                state.isDarkTheme, state.numberOfRowInTable,
+                state.areaColor, state.hitColor, state.missColor, state.backgroundColor)))
+        },
+        setMissColor(state, color){
+            state.missColor = color
+            localStorage.setItem('settings', JSON.stringify(new Customization(
+                state.isDarkTheme, state.numberOfRowInTable,
+                state.areaColor, state.hitColor, state.missColor, state.backgroundColor)))
+        },
+        setBackgroundColor(state, color){
+            state.backgroundColor = color
+            localStorage.setItem('settings', JSON.stringify(new Customization(
+                state.isDarkTheme, state.numberOfRowInTable,
+                state.areaColor, state.hitColor, state.missColor, state.backgroundColor)))
+        }
+    },
+    getters: {
+        isDarkTheme(state){return state.isDarkTheme},
+        numberOfRowInTable(state){return state.numberOfRowInTable},
+        areaColor(state){return state.areaColor},
+        hitColor(state){return state.hitColor},
+        missColor(state){return state.missColor},
+        backgroundColor(state){return state.backgroundColor},
+    }
+}
